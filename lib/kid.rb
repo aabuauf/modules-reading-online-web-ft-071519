f3adce3c9636c5985df173f09ef866999632a8e9
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-29 14:42:33 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,9 +1,8 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative ''
 
 class Kid
-class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-end
 end
